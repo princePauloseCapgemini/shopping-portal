ce9a4ef0344c69aebc80876deaa42c61a3901617
Paulose <prince.paulose@capgemini.com>
2023-03-14 17:15:32 +0530

    products card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,15 @@
 import { FC, memo } from "react";
 
-import { Box, HStack, Image, Flex, Text } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Image,
+  Flex,
+  Text,
+  Icon,
+  Tooltip,
+} from "@chakra-ui/react";
+import { StarIcon } from "@chakra-ui/icons";
 
 import { ProductProps } from "../types";
 
@@ -13,11 +22,16 @@ export const Card: FC<CardProps> = memo(function Card({ item }) {
     <Flex justifyContent="center" alignItems="center" flexDirection="column">
       <Image w="auto" height="150px" src={item.image} objectFit="contain" />
       <Box p="4">
-        <HStack>
-          <Text fontSize="md" fontWeight="semibold">
-            {item.title}
-          </Text>
+        <HStack justifyContent="space-between">
+          <Tooltip label={item.title}>
+            <Text noOfLines={2} w="50%" fontSize="md" fontWeight="semibold">
+              {item.title}
+            </Text>
+          </Tooltip>
+          <Icon as={StarIcon} color="yellow" />
+          <Text>{`${item.rating.rate} (${item.rating.count})`}</Text>
         </HStack>
+        <Text fontSize="xs" mt="4" fontWeight="bold">{`$ ${item.price}`}</Text>
       </Box>
     </Flex>
   );
